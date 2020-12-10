9785bfdb0fa241a0b47f1f81693e31e349125258
Mark Brough <mark.brough@gmail.com>
2020-12-10 16:04:45 +0100

    Allow home page to be displayed

--- iati_datastore/iatilib/frontend/routes.py
+++ iati_datastore/iatilib/frontend/routes.py
@@ -15,10 +15,11 @@ def homepage():
 def error():
     return redirect(url_for('routes.docs', path='api/error/'))
 
+@routes.route('/docs/')
 @routes.route('/docs/<path:path>')
-def docs(path):
+def docs(path=''):
     folder = join('frontend', 'docs', 'dirhtml')
-    if path.endswith('/'):
+    if (path.endswith('/') or path==''):
         path += 'index.html'
     return send_from_directory(folder, path)
 
