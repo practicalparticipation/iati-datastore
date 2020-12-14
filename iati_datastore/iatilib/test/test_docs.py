2e213a58850d724a029696fdb67884b8f0567461
Andy Lulham <a.lulham@gmail.com>
2020-12-14 16:13:45 +0000

    Build docs before testing them

--- iati_datastore/iatilib/test/test_docs.py
+++ iati_datastore/iatilib/test/test_docs.py
@@ -1,13 +1,17 @@
+from os.path import dirname, realpath, join
+import subprocess
+
 from . import AppTestCase
 
 
-class ClientTestCase(AppTestCase):
+class TestDocs(AppTestCase):
     def setUp(self):
-        super(ClientTestCase, self).setUp()
+        super().setUp()
         self.client = self.app.test_client()
+        current_path = dirname(dirname(dirname(realpath(__file__))))
+        cwd = join(current_path, 'docs_source')
+        subprocess.run(['make', 'dirhtml'], cwd=cwd)
 
-
-class TestDocs(ClientTestCase):
     def test_homepage_redirect(self):
         resp = self.client.get('/')
         self.assertEquals(302, resp.status_code)
@@ -23,7 +27,7 @@ class TestDocs(ClientTestCase):
         self.assertEquals(200, resp.status_code)
         self.assertRegex(
             resp.data.decode(),
-            r"What is the IATI Datastore\?")
+            r"What is IATI Datastore\?")
 
     def test_favicon(self):
         resp = self.client.get('/favicon.ico')
