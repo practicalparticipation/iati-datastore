60d97cbef0130d6a9972b9071ff84e3559a4c3c0
Andy Lulham <a.lulham@gmail.com>
2020-12-14 16:22:54 +0000

    Refactor a bit

--- iati_datastore/iatilib/test/test_docs.py
+++ iati_datastore/iatilib/test/test_docs.py
@@ -1,17 +1,25 @@
 from os.path import dirname, realpath, join
 import subprocess
 
-from . import AppTestCase
+from . import ClientTestCase
 
 
-class TestDocs(AppTestCase):
+class TestDocs(ClientTestCase):
     def setUp(self):
         super().setUp()
-        self.client = self.app.test_client()
         current_path = dirname(dirname(dirname(realpath(__file__))))
         cwd = join(current_path, 'docs_source')
         subprocess.run(['make', 'dirhtml'], cwd=cwd)
 
+    def test_docs(self):
+        resp = self.client.get('/docs/')
+        self.assertEquals(200, resp.status_code)
+        self.assertRegex(
+            resp.data.decode(),
+            r"What is IATI Datastore\?")
+
+
+class TestDocsRedirects(ClientTestCase):
     def test_homepage_redirect(self):
         resp = self.client.get('/')
         self.assertEquals(302, resp.status_code)
@@ -22,13 +30,8 @@ class TestDocs(AppTestCase):
         self.assertEquals(302, resp.status_code)
         self.assertRegex(resp.headers['Location'], '/docs/api/error/$')
 
-    def test_docs(self):
-        resp = self.client.get('/docs/')
-        self.assertEquals(200, resp.status_code)
-        self.assertRegex(
-            resp.data.decode(),
-            r"What is IATI Datastore\?")
 
+class TestFavicon(ClientTestCase):
     def test_favicon(self):
         resp = self.client.get('/favicon.ico')
         self.assertEquals(200, resp.status_code)
