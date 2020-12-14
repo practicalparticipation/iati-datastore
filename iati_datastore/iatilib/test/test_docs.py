0016f2b93d5b53f47f86ba61d889028696161c3e
Andy Lulham <a.lulham@gmail.com>
2020-12-14 15:41:10 +0000

    Add some docs tests

---
+++ iati_datastore/iatilib/test/test_docs.py
@@ -0,0 +1,26 @@
+from . import AppTestCase
+
+
+class ClientTestCase(AppTestCase):
+    def setUp(self):
+        super(ClientTestCase, self).setUp()
+        self.client = self.app.test_client()
+
+
+class TestDocs(ClientTestCase):
+    def test_homepage_redirect(self):
+        resp = self.client.get('/')
+        self.assertEquals(302, resp.status_code)
+        self.assertRegex(resp.headers['Location'], '/docs/$')
+
+    def test_error_api_redirect(self):
+        resp = self.client.get('/error/')
+        self.assertEquals(302, resp.status_code)
+        self.assertRegex(resp.headers['Location'], '/docs/api/error/$')
+
+    def test_docs(self):
+        resp = self.client.get('/docs/')
+        self.assertEquals(200, resp.status_code)
+        self.assertRegex(
+            resp.data.decode(),
+            r"What is the IATI Datastore\?")
