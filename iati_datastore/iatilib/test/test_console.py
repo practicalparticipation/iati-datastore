733edc50b0b75f26993e680176e4083b0077fc37
Andy Lulham <a.lulham@gmail.com>
2020-12-14 19:01:02 +0000

    More console tests

--- iati_datastore/iatilib/test/test_console.py
+++ iati_datastore/iatilib/test/test_console.py
@@ -13,6 +13,18 @@ class ConsoleTestCase(AppTestCase):
     @mock.patch('subprocess.run')
     def test_build_docs(self, mock):
         command = 'make dirhtml'
-        result = self.runner.invoke(console.build_docs)
+        self.runner.invoke(console.build_docs)
         self.assertEquals(1, mock.call_count)
         self.assertEquals(mock.call_args.args[0], command.split(' '))
+
+
+    @mock.patch('iatilib.db.create_all')
+    def test_create_db(self, mock):
+        self.runner.invoke(console.create_database)
+        self.assertEquals(1, mock.call_count)
+
+
+    @mock.patch('iatilib.db.drop_all')
+    def test_drop_db(self, mock):
+        self.runner.invoke(console.drop_database)
+        self.assertEquals(1, mock.call_count)
