9e01e16ae7edf11364d02f3d7f0eaf809a85e147
Mark Brough <mark.brough@gmail.com>
2020-12-17 21:17:00 +0100

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <b-navbar type="light" variant="light">
+      <b-container>
+        <b-navbar-brand href="#">
+          <a href="https://codeforiati.org">
+            <img
+              width="100px"
+              src="https://codeforiati.org/assets/img/logo.png"
+              title="A project of Code for IATI" />
+          </a>
+        </b-navbar-brand>
+      </b-container>
+    </b-navbar>
+    <nuxt />
+    <footer class="bg-light p-4">
+      <b-container>
+        <b-row>
+          <b-col>
+            <p>Based on the original <a href="http://datastore.iatistandard.org/query/">Datastore Query Builder</a>, which is free software licenced under the GNU General Public License.<br/>
+            <ul>
+              <li><a href="https://github.com/codeforIATI/datastore-query-builder">Datastore Query Builder on GitHub</a></li>
+              <li><a href="https://github.com/codeforIATI/IATI-Datastore">IATI Datastore Classic on Github</a></li>
+            </ul>
+            </p>
+          </b-col>
+          <b-col class="text-right">
+            IATI Datastore Classic is a project of <a href="https://codeforiati.org">Code for IATI</a>
+          </b-col>
+        </b-row>
+      </b-container>
+    </footer>
+  </div>
+</template>
+
+<style>
+body {
+  background-color: #ececec;
+}
+</style>
