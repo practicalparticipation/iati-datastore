43313e9d5687c620ac1dbcdfc0e2aadc11d97e45
Andy Lulham <a.lulham@gmail.com>
2021-01-08 12:03:16 +0000

    Add a failing stats test

--- iati_datastore/iatilib/test/test_model.py
+++ iati_datastore/iatilib/test/test_model.py
@@ -1,7 +1,7 @@
 from . import AppTestCase
 from . import factories as fac
 
-from iatilib.model import Activity, Resource
+from iatilib.model import Activity, Resource, Stats
 from iatilib import db
 
 
@@ -60,4 +60,27 @@ class TestResource(AppTestCase):
         self.assertEquals(
             0,
             Activity.query.filter_by(resource_url=res.url).count()
+        )
+
+    def test_stats_counts(self):
+        fac.ActivityFactory.create(
+            iati_identifier="47045-ARM-202-G05-H-00",
+            title="orig",
+        )
+        fac.ActivityFactory.create(
+            iati_identifier="47045-ARM-202-G05-H-01",
+            title="orig",
+        )
+        self.assertEquals(
+            2, Activity.query.count()
+        )
+        self.assertEquals(
+            2, Stats.query.filter_by(label='activities').first().count
+        )
+        db.session.query(Activity).delete()
+        self.assertEquals(
+            0, Activity.query.count()
+        )
+        self.assertEquals(
+            0, Stats.query.filter_by(label='activities').first().count
         )
