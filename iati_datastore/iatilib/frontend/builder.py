00c9acb21a5e2ba9d382c183aee28a5b88b37a1e
Andy Lulham <a.lulham@gmail.com>
2021-01-09 21:54:21 +0000

    Some /build fixes

--- iati_datastore/iatilib/frontend/builder.py
+++ iati_datastore/iatilib/frontend/builder.py
@@ -4,20 +4,26 @@ from flask import (abort, Blueprint, request, redirect, url_for)
 builder = Blueprint('builder', __name__)
 
 
-@builder.route('/<format>.csv')
-@builder.route('/<format>/<grouping>.csv')
-def builder_redirect(format, grouping=None):
-    format_lookup = ['activity', 'transaction', 'budget']
+@builder.route('/<breakdown>/')
+@builder.route('/<breakdown>.<format>')
+@builder.route('/<breakdown>/<grouping>.<format>')
+def builder_redirect(breakdown, grouping=None, format='json'):
+    breakdown_lookup = ['activity', 'transaction', 'budget']
     grouping_lookup = [None, 'by_sector', 'by_country']
-    if format not in format_lookup:
+    format_lookup = ['xml', 'json', 'csv']
+    if breakdown not in breakdown_lookup:
         return abort(404)
     if grouping not in grouping_lookup:
         return abort(404)
+    if format not in format_lookup:
+        return abort(404)
 
     options = dict(request.args)
-    if format != 'activity':
-        options['format'] = format
+    if breakdown != 'activity':
+        options['breakdown'] = breakdown
     if grouping is not None:
         options['grouping'] = '/' + grouping
+    if format != 'xml':
+        options['format'] = format
 
     return redirect(url_for('routes.homepage', **options))
