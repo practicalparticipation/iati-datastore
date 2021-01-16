3b26e8b2bcda6c5d939c76c16ca8f91c58a7c4f2
Andy Lulham <a.lulham@gmail.com>
2021-01-16 16:17:48 +0000

    Refactor (no need for hack with current Flask)

--- iati_datastore/iatilib/frontend/api1.py
+++ iati_datastore/iatilib/frontend/api1.py
@@ -378,20 +378,15 @@ class BudgetsBySectorView(DataStoreCSVView):
     serializer = staticmethod(serialize.csv_budget_by_sector)
 
 
-# Must declare this, instead of creating it twice,
-# to avoid the anti-duplication errors of Flask 0.10
-activity_view = ActivityView.as_view('activity')
-
 api.add_url_rule(
     '/access/activity/',
     defaults={"format": "json"},
-    view_func=activity_view,
-    endpoint="activity-view"
+    view_func=ActivityView.as_view('activity-view'),
 )
 
 api.add_url_rule(
     '/access/activity.<format>',
-    view_func=activity_view
+    view_func=ActivityView.as_view('activity')
 )
 
 api.add_url_rule(
