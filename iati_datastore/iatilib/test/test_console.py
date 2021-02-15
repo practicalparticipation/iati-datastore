4f7ee94a566232c7fa5eb4f80ceb2a558efda3a7
Andy Lulham <a.lulham@gmail.com>
2021-02-15 16:20:39 +0000

    Add a console test

--- iati_datastore/iatilib/test/test_console.py
+++ iati_datastore/iatilib/test/test_console.py
@@ -9,26 +9,35 @@ class ConsoleTestCase(AppTestCase):
         super().setUp()
         self.runner = self.app.test_cli_runner()
 
+    def test_cli(self):
+        result = self.runner.invoke(console.cli)
+        self.assertEquals(0, result.exit_code)
+        self.assertIn('Usage: ', result.output)
+
     @mock.patch('subprocess.run')
     def test_build_docs(self, mock):
         command = 'make dirhtml'
-        self.runner.invoke(console.build_docs)
+        result = self.runner.invoke(console.build_docs)
+        self.assertEquals(0, result.exit_code)
         self.assertEquals(1, mock.call_count)
         self.assertEquals(mock.call_args.args[0], command.split(' '))
 
     @mock.patch('click.confirm')
     @mock.patch('iatilib.db.drop_all')
     def test_drop_db(self, prompt_mock, drop_all_mock):
-        self.runner.invoke(console.drop_database)
+        result = self.runner.invoke(console.drop_database)
+        self.assertEquals(0, result.exit_code)
         self.assertEquals(1, prompt_mock.call_count)
         self.assertEquals(1, drop_all_mock.call_count)
 
     @mock.patch('iatilib.rq.get_queue')
     def test_status_cmd(self, rq_mock):
-        self.runner.invoke(crawler.status_cmd)
+        result = self.runner.invoke(crawler.status_cmd)
+        self.assertEquals(0, result.exit_code)
         self.assertEquals(1, rq_mock.call_count)
 
     @mock.patch('iatikit.download')
     def test_download_cmd(self, iatikit_mock):
-        self.runner.invoke(crawler.download_cmd)
+        result = self.runner.invoke(crawler.download_cmd)
+        self.assertEquals(0, result.exit_code)
         self.assertEquals(1, iatikit_mock.data.call_count)
