103a724a5ca2159040c1f48254c66329c26b110e
Mark Brough <mark.brough@gmail.com>
2021-11-03 00:47:33 +0000

    Only check out package-lock.json for now

--- fabfile.py
+++ fabfile.py
@@ -15,7 +15,7 @@ def virtualenv(conn):
 def deploy(conn):
     with virtualenv(conn):
         # check out latest version of repository
-        conn.run('git checkout -- .')
+        conn.run('git checkout iati_datastore/query_builder_source/package-lock.json')
         # pull latest copy of code in version control
         conn.run('git pull origin main')
         # install dependencies
